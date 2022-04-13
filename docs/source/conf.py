d33cdfaa634b142c5e4c9efedf17010b1496b1d5
Rafael Escudero Robles <rafa_escudero_99@rocketmail.com>
2022-04-13 17:13:43 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ARTF Licencias'
+copyright = '2022, ARTF'
+author = 'Rafael Escudero'
 
 release = '0.1'
 version = '0.1.0'
